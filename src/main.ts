7450f82156fc628e25d8e6b7495c4ea46a3b24f2
Vinícius Beserra <viniciusb.nitronews@gmail.com>
2024-12-03 14:41:55 -0300

    feat: corrigindo erro cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar validação global
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
