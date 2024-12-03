ed3efbc9839b49fbc6dd576ef7074bddd51adaf0
Vinícius Beserra <viniciusb.nitronews@gmail.com>
2024-12-03 10:00:38 -0300

    feat: remocao de comentários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-// import { SidebarProvider } from "@/components/ui/sidebar";
-// import { AppSidebar } from "@/components/app-sidebar";
 import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  return (
-    // <SidebarProvider>
-    //   <AppSidebar />
-    //   <main>{children}</main>
-    // </SidebarProvider>
-    <Dashboard />
-  );
+  return <Dashboard />;
 }
